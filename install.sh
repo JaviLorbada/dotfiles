9b4b11feb8bc58ccc325b6b7355d5b7565ba600d
JaviLorbada <javugi@gmail.com>
2014-11-16 02:01:21 +0100

    Added install Xcode snippets script

--- install.sh
+++ install.sh
@@ -11,3 +11,29 @@ do
         ln -s $link_from $link_to
     fi
 done
+
+source ~/.bash_profile
+
+SNIPPETS_URL=https://github.com/JaviLorbada/JLXcode-Snippets.git
+XCODE_SNIPPETS_DIR="${HOME}/Library/Developer/Xcode/UserData/CodeSnippets/"
+REPONAME=$(echo $SNIPPETS_URL | awk -F/ '{print $NF}' | sed -e 's/.git$//');
+
+cd ..
+if [ ! -d "${REPONAME}" ]; then
+    git clone $SNIPPETS_URL $REPONAME;
+    cd $REPONAME;
+else
+    cd $REPONAME;
+    git pull origin master
+fi
+
+# Make sure xcode snippets dir exists
+if [ ! -d "${XCODE_SNIPPETS_DIR}" ]; then
+    mkdir -p "${XCODE_SNIPPETS_DIR}"
+fi
+
+for file in *.codesnippet
+do
+    echo "Linking $file"
+    ln -s $file "$HOME/Library/Developer/Xcode/UserData/CodeSnippets"
+done
